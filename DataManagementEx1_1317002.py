b9c3b3046377305059d07ca52e47512364002870
Elster07 <136040147+Elster07@users.noreply.github.com>
2023-06-09 07:32:52 +0200

    Dice game: initial revision

---
+++ DataManagementEx1_1317002.py
@@ -0,0 +1,15 @@
+# Viktoria Kimmig 1317002 Exercise 1
+
+import random
+
+#Die 1
+a = random.randint(1,6)
+#Die 2
+b = random.randint(1,6)
+#Total value
+t = a+b
+
+print ("Rolling dice...")
+print ("Die 1:", a)
+print ("Die 2:", b)
+print ("Total value:", t)
